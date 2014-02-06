456431319b3dc51a63f586161bc8eb71d750b068
unknown <bmrobins@BMROBINS-090225.cgifederal.com>
2014-02-06 15:28:35 -0500

    updating send_email

--- send_email.pl
+++ send_email.pl
@@ -3,4 +3,4 @@
 my $recipient = $ARGV[0];my $subject = $ARGV[1];my $message = $ARGV[2];my $attachment = $ARGV[3];
 if($recipient eq '') {	print "Error: Please specify a recipient\n";	exit -1;} elsif($subject eq '') {	print "Error: Please specify a subject\n";	exit -1;} elsif($message eq '') {	print "Error: Please specify a message\n";	exit -1;}
 $sender = new Mail::Sender {	smtp => 'YOUR.SMTP.HERE',	from => 'your.address@mail.com',	};
-if($attachment ne ''){	$sender->MailFile({		to=>$recipient,		subject=>$subject,		msg=>$message,		file=>$attachment,		#debug=>'email_log.log'	});} else {	$sender->MailMsg({		to=>$recipient,		subject=>$subject,		msg=>$message,		#debug=>'email_log.log'	});}
+if($attachment ne ''){	$sender->MailFile({		to=>$recipient,		subject=>$subject,		msg=>$message,		file=>$attachment,		debug=>'email_log.log'	});} else {	$sender->MailMsg({		to=>$recipient,		subject=>$subject,		msg=>$message,		debug=>'email_log.log'	});}
