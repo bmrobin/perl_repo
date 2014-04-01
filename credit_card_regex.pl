70d6b6fd7eaef002ed2e16deed6dafddae9209fe
bmrobin <bmrobin@g.clemson.edu>
2014-04-01 10:06:56 -0400

    added regexes for other CCs. discover card doesn't work yet

--- credit_card_regex.pl
+++ credit_card_regex.pl
@@ -8,8 +8,11 @@ my $cc_number = <>;
 chomp($cc_number);
 
 switch ($cc_number) {
+	case /[^\d]/ {print "credit card must contain numbers only";}
 	case /^34|^37/ {print "American Express\n";}
-	case /^62|^88/ {print "China UnionPay\n";}
-	
+	#case /^(6011|622[126]|64[4-9]|65)/ {print "Discover Card";}
+	case /^62{1}|^88/ {print "China UnionPay\n";}
+	case /^30[0-5]|^309|^36|^38|^39/ {print "Diners Club [something]\n";}
+	case /^54|^55/ {print "Diners Club US & Canada";}
 	else {print "unrecognized card\n";}
 }
