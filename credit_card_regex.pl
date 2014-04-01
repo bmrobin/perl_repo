f2c7a936f7e68e2a94ecd69e329b7f6a235084b4
bmrobin <bmrobin@g.clemson.edu>
2014-04-01 17:07:32 -0400

    added remaining cards

--- credit_card_regex.pl
+++ credit_card_regex.pl
@@ -16,8 +16,15 @@ while ($cc_number ne "exit") {
 		case /^34|^37/ {print "American Express\n";}
 		case /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[0-2][0-5])|64[4-9]|65)/ {print "Discover Card\n";}
 		case /^62|^88/ {print "China UnionPay\n";}
-		case /^30[0-5]|^309|^36|^38|^39/ {print "Diners Club [something]\n";}
+		case /^30[0-5]|^309|^36|^3[8-9]/ {print "Diners Club International\n";}
 		case /^54|^55/ {print "Diners Club US & Canada\n";}
+		case /^35(2[8-9]|[3-8][0-9])/ {print "JCB\n";}
+		case /^(6304|6706|6771|6709)/ {print "Laser\n";}
+		case /^(5018|5020|5038|5612|5893|6304|6759|6761|6762|6763|0604|6390)/ {print "Maestro\n";}
+		case /^5019/ {print "Dankort\n";}
+		case /^5[0-5]/ {print "Mastercard\n";}
+		case /^(4026|417500|4405|4508|4844|4913|4917)/ {print "Visa Electron\n";}
+		case /^4/ {print "Visa\n";}
 		else {print "unrecognized card\n";}
 	}
 }
