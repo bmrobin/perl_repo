0406cb68025b35e34e852e513b94624e7c9d3ea1
bmrobin <bmrobin@g.clemson.edu>
2014-04-01 16:47:39 -0400

    added loop for testing; discover card working

--- credit_card_regex.pl
+++ credit_card_regex.pl
@@ -3,16 +3,21 @@ use strict;
 use warnings;
 use Switch;
 
-print "Enter credit card number:";
-my $cc_number = <>;
-chomp($cc_number);
+my $cc_number = "";
 
-switch ($cc_number) {
-	case /[^\d]/ {print "credit card must contain numbers only";}
-	case /^34|^37/ {print "American Express\n";}
-	#case /^(6011|622[126]|64[4-9]|65)/ {print "Discover Card";}
-	case /^62{1}|^88/ {print "China UnionPay\n";}
-	case /^30[0-5]|^309|^36|^38|^39/ {print "Diners Club [something]\n";}
-	case /^54|^55/ {print "Diners Club US & Canada";}
-	else {print "unrecognized card\n";}
+while ($cc_number ne "exit") {
+	
+	print "Enter credit card number:";
+	$cc_number = <>;
+	chomp($cc_number);
+	
+	switch ($cc_number) {
+		case /[^\d]/ {print "credit card must contain numbers only\n";}
+		case /^34|^37/ {print "American Express\n";}
+		case /^(6011|622(12[6-9]|1[3-9][0-9]|[2-8][0-9][0-9]|9[0-2][0-5])|64[4-9]|65)/ {print "Discover Card\n";}
+		case /^62|^88/ {print "China UnionPay\n";}
+		case /^30[0-5]|^309|^36|^38|^39/ {print "Diners Club [something]\n";}
+		case /^54|^55/ {print "Diners Club US & Canada\n";}
+		else {print "unrecognized card\n";}
+	}
 }
